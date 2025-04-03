ebb3e2019aba6b9cb8af6470bea9114eb3f29922
vanitha <example@gmail.com>
2025-04-03 10:43:38 +0530

    Dbcontext issues

--- obj/Debug/net9.0/MovieAppBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/MovieAppBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAppBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27c9faa5b25bb886aaedbac4ebd88c61f1d3b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAppBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAppBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
